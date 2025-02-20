c62df9fd1001d5c823629da045ab446439e6ba41
magic3xx <darkmagic3xx@gmail.com>
2025-02-20 13:46:11 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 export const CONSUMET_URL = "https://oyasumi-api-host.vercel.app/anime/zoro";
 
-export const CONSUMET_URL_2 = "https://oyasumi-api-host.vercel.app/anime/gogoanime";
+export const CONSUMET_URL_2 = "https://api.kitsunee.me/v2/hianime/home";
